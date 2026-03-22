e8faa168e3bfd9506a7a7f3ffda15a098ee1d34b
Felippe Ribeiro <felipper43@gmail.com>
2026-03-22 12:38:44 -0300

    WIP: omit fields in user route

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,11 +1,11 @@
 package model
 
 type User struct {
-	Id       int    `json:"id" db:"id"`
+	Id       int    `json:"-" db:"id"`
 	Name     string `json:"name" db:"name"`
 	Email    string `json:"email" db:"email"`
 	IsAdmin  bool   `json:"is_admin" db:"is_admin"`
-	Password string `json:"password" db:"password"`
+	Password string `json:"-" db:"password"`
 }
 
 type CreateUser struct {
